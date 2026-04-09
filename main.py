d954cac9622aaaea6eff996250b69eff288ebe39
Ramroyal123 <rambabuevana1898@gmail.com>
2026-04-10 01:44:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import os
 import requests
 from twilio.rest import Client
 OWM_ENDPOINT="http://api.openweathermap.org/data/2.5/forecast"
+YOUR_LATITUDE=16.596148
+YOUR_LONGITUDE=81.592646
 api_key=os.environ.get("API_KEY")
 
 account_sid=os.environ.get("ACCOUNT_SID")
 auth_token=os.environ.get("AUTH_TOKEN")
 
 weather_parameters={
-    "lat": YOUR_LATITUDE, #16.596148,
-    "lon":YOUR_LONGITUDE, #81.592646,
+    "lat": YOUR_LATITUDE,
+    "lon":YOUR_LONGITUDE,
     "appid":api_key,
     "cnt":4
 }
